1225e29eae39a42133e6cfd576f7ef0cbda44ee0
Ancocodet <info@ancozockt.de>
2022-08-19 17:03:42 +0200

    Start User Implementation

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -32,7 +32,7 @@ class Service
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $adress;
+    private $address;
 
     /**
      * @ORM\Column(type="boolean")
@@ -100,14 +100,14 @@ class Service
         return $this;
     }
 
-    public function getAdress(): ?string
+    public function getAddress(): ?string
     {
-        return $this->adress;
+        return $this->address;
     }
 
-    public function setAdress(string $adress): self
+    public function setAddress(string $address): self
     {
-        $this->adress = $adress;
+        $this->address = $address;
 
         return $this;
     }
