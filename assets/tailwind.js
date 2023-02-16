5c0841758de2b659e486782f0aa5dcacdf019ab9
Ancocodet <dseydel@united-internet.de>
2023-02-16 10:25:47 +0100

    Update styling to look cleaner

--- assets/tailwind.js
+++ assets/tailwind.js
@@ -1,7 +1,13 @@
 import './styles/tailwind.less';
+import 'tippy.js/dist/tippy.css';
 
 import Alpine from 'alpinejs'
+import Tooltip from "@ryangjchandler/alpine-tooltip";
+
+Alpine.plugin(Tooltip);
+
 window.Alpine = Alpine
 Alpine.start()
 
 import './scripts/init-alpine.js'
+
