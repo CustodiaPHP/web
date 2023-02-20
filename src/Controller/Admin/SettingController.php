59ebe92350e69743b8efd4d40d0da79a5f84c4de
Ancocodet <dseydel@united-internet.de>
2023-02-20 10:24:58 +0100

    Force https and add notification settings

--- src/Controller/Admin/SettingController.php
+++ src/Controller/Admin/SettingController.php
@@ -14,7 +14,7 @@ class SettingController extends AbstractController
 {
 
     #[Route('/general', name: 'app_admin_setting_general', methods: ['GET', 'POST'])]
-    public function index(Request $request, SettingRepository $settingRepository): Response
+    public function general(Request $request, SettingRepository $settingRepository): Response
     {
 		if(!$this->isGranted('ROLE_SUPER_ADMIN')){
 			return $this->redirectToRoute('app_admin_dashboard');
@@ -22,8 +22,10 @@ class SettingController extends AbstractController
 
 		if ($request->getMethod() === 'POST') {
 			foreach (SettingsHelper::SETTINGS as $setting) {
-				$value = $this->sanitizeSetting($setting, (array) $request->request->get($setting));
-				$settingRepository->update($setting, $value);
+				if($request->request->has($setting)) {
+					$value = $this->sanitizeSetting($setting, (array) $request->request->get($setting));
+					$settingRepository->update($setting, $value);
+				}
 			}
 			$settingRepository->flush();
 		}
@@ -32,6 +34,28 @@ class SettingController extends AbstractController
             'settings' => $settingRepository,
         ]);
     }
+
+	#[Route('/notifications', name: 'app_admin_setting_notifications', methods: ['GET', 'POST'])]
+	public function notifications(Request $request, SettingRepository $settingRepository): Response
+	{
+		if(!$this->isGranted('ROLE_SUPER_ADMIN')){
+			return $this->redirectToRoute('app_admin_dashboard');
+		}
+
+		if ($request->getMethod() === 'POST') {
+			foreach (SettingsHelper::SETTINGS as $setting) {
+				if($request->request->has($setting)) {
+					$value = $this->sanitizeSetting($setting, (array) $request->request->get($setting));
+					$settingRepository->update($setting, $value);
+				}
+			}
+			$settingRepository->flush();
+		}
+
+		return $this->render( 'admin/setting/notifications.html.twig', [
+			'settings' => $settingRepository,
+		]);
+	}
 
 	private function sanitizeSetting(string $key, array $value) : array
 	{
